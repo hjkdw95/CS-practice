79e20aeb238b08ae7071614bd052f6c454712455
Casey <hjkdw9506@gmail.com>
2021-07-15 10:44:27 +0900

    ✅ 6749 문제풀이

---
+++ 알고리즘/입출력과 사칙연산/6749_nextInLine.js
@@ -0,0 +1,21 @@
+// 문제
+// You know a family with three children. Their ages form an arithmetic sequence: the difference in ages between the middle child and youngest child is the same as the difference in ages between the oldest child and the middle child. For example, their ages could be 5, 10 and 15, since both adjacent pairs have a difference of 5 years.
+
+// Given the ages of the youngest and middle children, what is the age of the oldest child?
+
+// 입력
+// The input consists of two integers, each on a separate line. The first line is the age Y of the youngest child (0 ≤ Y ≤ 50). The second line is the age M of the middle child (Y ≤ M ≤ 50).
+
+// 출력
+// The output will be the age of the oldest child.
+
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let youngest = Number(input[0]);
+let middle = Number(input[1]);
+
+let oldest = middle - youngest + middle;
+
+console.log(oldest);
