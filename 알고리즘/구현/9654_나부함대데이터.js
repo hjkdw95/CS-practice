36602a146ba8596bed8c5e7621fe4ff36f16639f
Casey <hjkdw9506@gmail.com>
2021-07-21 08:14:31 +0900

    ✅ 9654 문제 풀이

---
+++ 알고리즘/구현/9654_나부함대데이터.js
@@ -0,0 +1,17 @@
+// 문제
+// 나부 행성의 함대 정보를 출력하는 프로그램을 작성하시오.
+
+// 출력
+// 나부 행성의 함대의 정보를 아래와 예제 출력과 같은 표로 출력한다.
+
+// 처음 두 열의 너비는 문자 15개, 세 번째 열은 11개, 마지막 열의 너비는 10개이다.
+
+console.log("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+console.log("N2 Bomber      Heavy Fighter  Limited    21        ");
+console.log("J-Type 327     Light Combat   Unlimited  1         ");
+console.log("NX Cruiser     Medium Fighter Limited    18        ");
+console.log("N1 Starfighter Medium Fighter Unlimited  25        ");
+console.log("Royal Cruiser  Light Combat   Limited    4         ");
+
+/* 해설
+이것도 너무 어렵게 생각하지 말것. 단순 출력문제이다! */
